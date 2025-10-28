10217a8bbce9d5578c1488977faeefdc5b84956e
eglandinselmani <eglandinselmani1@gmail.com>
2025-10-28 20:47:27 +0100

    variables

---
+++ lesson1/variables.py
@@ -0,0 +1,24 @@
+temperatura = 98.6
+name =  "olti"
+
+print(type(temperatura))
+print(type(name))
+
+
+age = 30
+print("age is", age)
+
+
+x = 5
+y = 3
+result = x + y
+print(result)
+
+
+age = age + 1
+print("age now is:", age)
+
+first_name = "driart"
+last_name = ("bytyqi")
+fullname = first_name + " " + last_name
+print("fullname")
